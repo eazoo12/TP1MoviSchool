c2ebe594fa144517b29b809319a9a0e6ceadb0bf
eazoo12 <eazoo15@gmail.com>
2019-04-05 01:38:07 -0500

    Cadena Conexion

---
+++ WebMoviSchool/Capa Datos/datConexion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Capa_Datos
+{
+    class datConexion
+    {
+        
+        public static string cad_con = @"Server=.;Database=BD_MOVISCHOOL; Integrated Security=True;";
+
+
+    }
+}
